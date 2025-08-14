1fe4b522048a7ef03afb91b4926e53772558de69
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2025-08-13 19:43:36 -0700

    fix background and scoring

--- scripts/fall_area.gd
+++ scripts/fall_area.gd
@@ -14,3 +14,4 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "Player"):
 		get_tree().reload_current_scene()
+		GameController.reset()
--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -5,3 +5,6 @@ var total_coins: int = 0
 func coin_collected(value: int):
 	total_coins += value
 	EventController.emit_signal("coin_collected", total_coins)
+func reset():
+	total_coins = 0
+	EventController.emit_signal("coin_collected", total_coins)
